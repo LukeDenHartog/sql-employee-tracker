2395d836659b0c7b5164dfb9b223e31b94a471a8
Luke <121516961+Afrozez@users.noreply.github.com>
2023-06-25 16:13:26 -0700

    removed express and created a database

---
+++ db/shcema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employee_tracker;
+
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+
+DROP TABLE IF EXISTS departments_db;
+
+
+
+CREATE TABLE departments_db (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+
+
--- index.js
+++ index.js
@@ -14,26 +14,38 @@ inquirer
        // create a statement to view all employees
     }
 
-        if(answers.index === 'Update Employee Role') {
-            // create a statement to update employee role
+    if (answers.index === 'Update Employee Role') {
+        // create a statement to update employee role
+    } else if (answers.index === 'View All Roles') {
+        // create a statement to view all roles
+    } else if (answers.index === 'Add Role') {
+        inquirer.prompt([
+            {
+            type: 'input',
+            name: 'roleNameInput',
+            message: 'What is the name of the role?'
+            }
+        ]).then((answer) => {
+            // create a statement to add a department
+            let roleName;
+            answer.roleNameInput = roleName;
+        });
+        // create a statement to add a role
+    } else if (answers.index === 'View All Departments') {
+        // create a statement to view all departments
+    } else if (answers.index === 'Add Department') {
+        inquirer.prompt([
+        {
+        type: 'input',
+        name: 'departmentNameInput',
+        message: 'What is the name of the department?'
         }
-        if(answers.index === 'View All Roles') {
-            // create a statement to view all roles
-        }
-        if(answers.index === 'Add Role') {
-            // create a statement to add a role
-        }
-
-        if(answers.index === 'View All Departments') {
-            // create a statement to view all departments
-        }
-
-        if(answers.index === 'Add Department') {
-            // create a statement to add a department.
-        }
-
-        if(answers.index === 'Quit') {
-            console.log('Please ')
-            // create a statement to quit the command line questions
-        }
+    ]).then((answers) => {
+        
+        // create a statement to add a department
+    });
+    } else if (answers.index === 'Quit') {
+        console.log('Please run "node index.js to start');
+        // create a statement to quit the command line questions
+    }
     });
