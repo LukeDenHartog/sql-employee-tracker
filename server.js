b6f2f961a8ce2114698a305b00af9b93181efb04
Luke <121516961+Afrozez@users.noreply.github.com>
2023-06-27 18:21:26 -0700

    created INNER JOINS to display multiple items on one table

--- server.js
+++ server.js
@@ -33,163 +33,157 @@ db.query("SELECT * FROM employee", (error, response) => {
 displayPrompt();  // Starts the terminal prompt.
 
 function displayPrompt() {
-inquirer
-    .prompt([
-        {
+    inquirer.prompt([
+      {
         type: 'list',
         name: 'index',
         message: 'What would you like to do?',
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
-        }
+      }
     ]).then((answers) => {
-    /*---------------- If user selected 'View All Employees' the code below will run.----------------*/
-        if (answers.index === 'View All Employees') {
-          db.query("SELECT * FROM employee", function(err, res) {
-            if (err) throw err;
-            
-            // Remove the index column from the results
-            const tableData = res.map(({ index, ...rest }) => Object.values(rest));
-        
-            // Create a new table array
-            const data = [];
-            
-            // Add headers to the table
-            const headers = Object.keys(res[0]).filter(key => key !== 'index');
-            data.push(headers);
-            
-            // Add rows to the table
-            tableData.forEach(row => data.push(row));
-            
-            // Display the table
-            const output = table(data);
-            console.log(output);
-            displayPrompt();
-          });
-        }
-    if (answers.index === 'Add Employee') {
-
+      /*---------------- If user selected 'View All Employees' the code below will run.----------------*/
+      if (answers.index === 'View All Employees') {
+        db.query("SELECT employee.first_name, employee.last_name, role.title, departments.name AS department, role.salary FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN departments ON role.department_id=departments.id;", function (err, res) {
+          if (err) throw err;
+  
+          // Remove the index column from the results
+          const tableData = res.map(({ index, ...rest }) => Object.values(rest));
+  
+          // Create a new table array
+          const data = [];
+  
+          // Add headers to the table
+          const headers = Object.keys(res[0]).filter(key => key !== 'index');
+          data.push(headers);
+  
+          // Add rows to the table
+          tableData.forEach(row => data.push(row));
+  
+          // Display the table
+          const output = table(data);
+          console.log(output);
+          displayPrompt();
+        });
+      }
+      if (answers.index === 'Add Employee') {
         db.query("SELECT id, title FROM role", (error, results) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-    
-            const newRolesArray = results.map(({ id, title }) => ({
-                name: title,
-                value: id,
-            }));
-            console.log(newRolesArray)
-        inquirer.prompt([
+          if (error) {
+            console.error(error);
+            return;
+          }
+  
+          const newRolesArray = results.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+          inquirer.prompt([
             {
-                type: 'input',
-                name: 'newFirstName',
-                message: 'What is the first name of the new employee?'
+              type: 'input',
+              name: 'newFirstName',
+              message: 'What is the first name of the new employee?'
             },
             {
-                type: 'input',
-                name: 'newLastName',
-                message: 'What is the last name of the new employee?'
+              type: 'input',
+              name: 'newLastName',
+              message: 'What is the last name of the new employee?'
             },
             {
-                type: 'list',
-                name: 'roleNameInput',
-                message: 'Which role would you like to assign to this employee?',
-                choices: newRolesArray
+              type: 'list',
+              name: 'roleNameInput',
+              message: 'Which role would you like to assign to this employee?',
+              choices: newRolesArray
             }
-         
-        ]).then((answers) => {
+  
+          ]).then((answers) => {
             db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
-            VALUE ("${answers.newFirstName}", "${answers.newLastName}", ${answers.roleNameInput}, null )`)
+              VALUE ("${answers.newFirstName}", "${answers.newLastName}", ${answers.roleNameInput}, null )`)
+          })
         })
-    })
-    } 
-   
-    //db.query(`INSERT INTO departments (name) VALUES ("${answers.depNameInput}")`, (err, res)
-/*---------------- If user selected 'Update Employee Role' the code below will run.----------------*/
-    if (answers.index === 'Update Employee Role') {
-      
+      }
+  
+      /*---------------- If user selected 'Update Employee Role' the code below will run.----------------*/
+      if (answers.index === 'Update Employee Role') {
+  
         inquirer.prompt([
-            {
+          {
             type: 'list',
             name: 'employeeChoice',
             message: "Which employee's role would you like to update?",
             choices: employeeNames
-            },
-            {
+          },
+          {
             type: 'list',
             name: 'roleNameInput',
             message: 'Which role would you like to assign to this employee?',
             choices: dynamicRoleList
-            }
-            ]).then((answer) => {
-                
-            // create a statement to add a department.
-           
+          }
+        ]).then((answer) => {
+  
+          // create a statement to add a department.
+  
         });
         // create a statement to update employee role.
-    } else if (answers.index === 'View All Roles') {
+      } else if (answers.index === 'View All Roles') {
         // create a statement to view all roles.
-    } else if (answers.index === 'Add Role') {
+      } else if (answers.index === 'Add Role') {
         db.query("SELECT * FROM departments", (error, response) => {
-           
-        inquirer.prompt([
+  
+          inquirer.prompt([
             {
-            type: 'input',
-            name: 'roleNameInput',
-            message: 'What is the name of the role?'
+              type: 'input',
+              name: 'roleNameInput',
+              message: 'What is the name of the role?'
             },
             {
-                type: 'input',
-                name: 'salaryInput',
-                message: 'What is the salary of the role?'
+              type: 'input',
+              name: 'salaryInput',
+              message: 'What is the salary of the role?'
             },
             {
-                type: 'list',
-                name: 'departmentInput',
-                message: 'Which department does the role belong to?',
-                choices: dynamicDepartmentList
+              type: 'list',
+              name: 'departmentInput',
+              message: 'Which department does the role belong to?',
+              choices: dynamicDepartmentList
             }
-        ]).then((answer) => {
+          ]).then((answer) => {
             db.query(`INSERT INTO departments (name) VALUES ("${answers.depNameInput}")`, (err, res) => {
-                if (err) throw err;
-                console.log('Added new department successfully');
-                displayPrompt();  // Starts the terminal prompt.
+              if (err) throw err;
+              console.log('Added new department successfully');
+              displayPrompt(); // Starts the terminal prompt.
             })
-            answer.roleNameInput
+          });
         });
-      
-    });
-    } 
-
-    /*---------------- If user selected 'Update Employee Role' the code below will run. ----------------*/
-    else if (answers.index === 'View All Departments') {
+      }
+  
+      /*---------------- If user selected 'Update Employee Role' the code below will run. ----------------*/
+      else if (answers.index === 'View All Departments') {
         db.query(`SELECT * FROM departments;`, (err, res) => {
-         console.table(res);
-         displayPrompt();  // Starts the terminal prompt.
+          console.table(res);
+          displayPrompt(); // Starts the terminal prompt.
         });
-        
-
-          /*---------------- If user selected 'Add Department' the code below will run. ----------------*/
-    } else if (answers.index === 'Add Department') {
-      
+  
+  
+        /*---------------- If user selected 'Add Department' the code below will run. ----------------*/
+      } else if (answers.index === 'Add Department') {
+  
         inquirer.prompt([
-        {
-        type: 'input',
-        name: 'depNameInput',
-        message: 'What is the name of the department?'
-        }
-    ]).then((answers) => {  
-        db.query(`INSERT INTO departments (name) VALUES ("${answers.depNameInput}")`, (err, res) => {
+          {
+            type: 'input',
+            name: 'depNameInput',
+            message: 'What is the name of the department?'
+          }
+        ]).then((answers) => {
+          db.query(`INSERT INTO departments (name) VALUES ("${answers.depNameInput}")`, (err, res) => {
             if (err) throw err;
             console.log(`Added ${answers.depNameInput} to the database`);
-            displayPrompt();  // Starts the terminal prompt.
-        })
-    });
-
-    } else if (answers.index === 'Quit') {
+            displayPrompt(); // Starts the terminal prompt.
+          })
+        });
+  
+      } else if (answers.index === 'Quit') {
         console.log('Please run "node server.js" inside the terminal in order to start over');
         // create a statement to quit the command line questions.
-    }
+      }
     });
-
-}
+  }
+  
